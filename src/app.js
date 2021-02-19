ccaa3c38c1e15af6339cfd8df55013e4df704037
Rafael Vieira Costa <rafaelfilipecosta@gmail.com>
2021-02-19 19:58:17 +0000

    correcting the error

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs')
-const { response } = require('express');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 
 
 
@@ -53,13 +55,26 @@ app.get('/weather', (req, res) => {
         });
     }
 
-    res.send({
-        forecast: "sol",
-        location: "Porto",
-        address: req.query.city
+    geocode(req.query.city, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
 
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.city
+            })
+        })
     })
 });
+
+    
 
 app.get('*', (req, res) => {
     res.render('404', {})
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,7 @@
+const { response } = require('express');
 const request = require ('request');
 
-const geocode = (adress, callback) => {
+const geocode = (address, callback) => {
 
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address +'.json?access_token=pk.eyJ1IjoicnZjb3N0YSIsImEiOiJja2w4OTUyOGwwYmpzMnJuMHhndDAyenpiIn0.GW0KOwKBPq1-OLWSIIV2eA'
     request({  url, json:true}, (error, {body}) => {
